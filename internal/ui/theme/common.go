4d64bde5a6c26a66ab5f2ce191cfed023d5a425e
Markus Ressel <mail@markusressel.de>
2023-10-07 15:26:34 +0200

    theme refactoring

---
+++ internal/ui/theme/common.go
@@ -0,0 +1,60 @@
+package theme
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell/v2"
+)
+
+type HeaderColors struct {
+	Name           tcell.Color
+	NameBackground tcell.Color
+
+	PageIndicator           tcell.Color
+	PageIndicatorBackground tcell.Color
+
+	UpdateInterval           tcell.Color
+	UpdateIntervalBackground tcell.Color
+
+	Version           tcell.Color
+	VersionBackground tcell.Color
+}
+
+type DialogColors struct {
+	Border tcell.Color
+}
+
+type StyleStruct struct {
+	Layout LayoutStyle
+}
+
+type LayoutStyle struct {
+	TitleAlign       int
+	DialogTitleAlign int
+}
+
+type Color struct {
+	Header HeaderColors
+	Dialog DialogColors
+	Layout LayoutColors
+	Graphs GraphsColors
+}
+
+type GraphsColors struct {
+	Rpm    tcell.Color
+	Pwm    tcell.Color
+	Curve  tcell.Color
+	Sensor tcell.Color
+}
+
+type LayoutColors struct {
+	Border tcell.Color
+	Title  tcell.Color
+}
+
+func CreateTitleText(text string) string {
+	if len(text) <= 0 {
+		return ""
+	}
+	titleText := fmt.Sprintf(" %s ", text)
+	return titleText
+}
--- internal/ui/theme/dark.go
+++ internal/ui/theme/dark.go
@@ -1,7 +1,6 @@
 package theme
 
 import (
-	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
@@ -13,7 +12,9 @@ var (
 
 	OnPrimaryColor = tcell.ColorWhite
 	OnSecondary    = tcell.ColorBlack
+)
 
+var (
 	Colors = Color{
 		Header: HeaderColors{
 			Name:           OnPrimaryColor,
@@ -50,57 +51,3 @@ var (
 		},
 	}
 )
-
-type HeaderColors struct {
-	Name           tcell.Color
-	NameBackground tcell.Color
-
-	PageIndicator           tcell.Color
-	PageIndicatorBackground tcell.Color
-
-	UpdateInterval           tcell.Color
-	UpdateIntervalBackground tcell.Color
-
-	Version           tcell.Color
-	VersionBackground tcell.Color
-}
-
-type DialogColors struct {
-	Border tcell.Color
-}
-
-type StyleStruct struct {
-	Layout LayoutStyle
-}
-
-type LayoutStyle struct {
-	TitleAlign       int
-	DialogTitleAlign int
-}
-
-type Color struct {
-	Header HeaderColors
-	Dialog DialogColors
-	Layout LayoutColors
-	Graphs GraphsColors
-}
-
-type GraphsColors struct {
-	Rpm    tcell.Color
-	Pwm    tcell.Color
-	Curve  tcell.Color
-	Sensor tcell.Color
-}
-
-type LayoutColors struct {
-	Border tcell.Color
-	Title  tcell.Color
-}
-
-func CreateTitleText(text string) string {
-	if len(text) <= 0 {
-		return ""
-	}
-	titleText := fmt.Sprintf(" %s ", text)
-	return titleText
-}
