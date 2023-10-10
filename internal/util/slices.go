1a989740a2da84ef99d707273a1e7aabc2a436fb
Markus Ressel <mail@markusressel.de>
2023-10-11 00:00:11 +0200

    remove unused code

--- internal/util/slices.go
+++
@@ -1,9 +0,0 @@
-package util
-
-func RotateSliceLeft[T any](s []T) []T {
-	return append(s[1:], s[0])
-}
-
-func RotateSliceRight[T any](s []T) []T {
-	return append([]T{s[len(s)-1]}, s[:len(s)-1]...)
-}
